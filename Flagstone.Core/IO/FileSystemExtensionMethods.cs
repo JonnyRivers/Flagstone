f9f04e8acc4e61af5c5e1cfa1f2a6980943ff2ea
JonnyRivers <jonny.w.rivers@gmail.com>
2016-02-20 17:03:11 -0800

    Added extension methods for IFileSystem

---
+++ Flagstone.Core/IO/FileSystemExtensionMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagstone.IO
+{
+    public static class FileSystemExtensionMethods
+    {
+        public static void SafeCopy(this IFileSystem fileSystem, string sourcePath, string destinationPath, bool overwrite){
+            String destinationDirectory = fileSystem.Path.GetDirectoryName(destinationPath);
+            if (!fileSystem.Directory.Exists(destinationDirectory)) {
+                fileSystem.Directory.CreateDirectory(destinationDirectory);
+            }
+
+            fileSystem.File.Copy(sourcePath, destinationPath, overwrite);
+        }
+
+        public static bool IsFirstFileNewer(this IFileSystem fileSystem, string sourcePath, string destinationPath)
+        {
+            FileInfoBase sourceFileInfo = fileSystem.FileInfo.FromFileName(sourcePath);
+            FileInfoBase destinationFileInfo = fileSystem.FileInfo.FromFileName(destinationPath);
+
+            return (sourceFileInfo.LastWriteTime > destinationFileInfo.LastWriteTime);
+        }
+
+        public static string[] GetFileNamesRecursive(this IFileSystem fileSystem, string directory, string searchPattern)
+        {
+            var fileNames = new List<string>();
+            fileSystem.GetFileNamesRecursive(directory, searchPattern, fileNames);
+
+            return fileNames.ToArray();
+        }
+
+        private static void GetFileNamesRecursive(this IFileSystem fileSystem, string directory, string searchPattern, List<string> fileNames)
+        {
+            String[] subDirectories = fileSystem.Directory.GetDirectories(directory);
+            foreach (String subDirectory in subDirectories)
+            {
+                DirectoryInfoBase di = fileSystem.DirectoryInfo.FromDirectoryName(subDirectory);
+
+                // TODO: remove this deplorable hack
+                if (di.Name == "$RECYCLE.BIN" || di.Name == "System Volume Information")
+                    continue;
+
+                fileSystem.GetFileNamesRecursive(subDirectory, searchPattern, fileNames);
+            }
+
+            String[] files = fileSystem.Directory.GetFiles(directory);
+            if (files.Length > 0)
+                fileNames.AddRange(files);
+        }
+    }
+}
--- Testbeds/DriveBackup/Application.cs
+++ Testbeds/DriveBackup/Application.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Flagstone.IO;
 using Flagstone.Logger;
 
 namespace DriveBackup
@@ -22,7 +23,7 @@ namespace DriveBackup
 
         public int Run(string sourceRootDirectory, string destinationRootDirectory)
         {
-            string[] sourceFileNames = GetFileNamesRecursive(sourceRootDirectory, "*");
+            string[] sourceFileNames = m_fileSystem.GetFileNamesRecursive(sourceRootDirectory, "*");
             m_logger.Info("Found {0} files at source directory \"{1}\"", sourceFileNames.Length, sourceRootDirectory);
 
             foreach (string sourceFileName in sourceFileNames)
@@ -31,70 +32,20 @@ namespace DriveBackup
 
                 if (m_fileSystem.File.Exists(destinationFileName))
                 {
-                    // TODO: this is generally useful
-                    // TODO: Think about how we extend IFileSystem
-                    FileInfoBase sourceFileInfo = m_fileSystem.FileInfo.FromFileName(sourceFileName);
-                    FileInfoBase destinationFileInfo = m_fileSystem.FileInfo.FromFileName(destinationFileName);
-
-                    if (sourceFileInfo.LastWriteTime > destinationFileInfo.LastWriteTime)
+                    if (m_fileSystem.IsFirstFileNewer(sourceFileName, destinationFileName))
                     {
                         m_logger.Info("Overwriting '{0}'", destinationFileName);
-
-                        // TODO: This block is duplicated.
-                        // TODO: Think about how we extend IFileSystem
-                        String destinationDirectory = m_fileSystem.Path.GetDirectoryName(destinationFileName);
-                        if (!m_fileSystem.Directory.Exists(destinationDirectory))
-                        {
-                            m_fileSystem.Directory.CreateDirectory(destinationDirectory);
-                        }
-                        
-                        m_fileSystem.File.Copy(sourceFileName, destinationFileName, true);
+                        m_fileSystem.SafeCopy(sourceFileName, destinationFileName, true);
                     }
                 }
                 else
                 {
                     m_logger.Info("Copying '{0}'", sourceFileName);
-
-                    // TODO: This block is duplicated.
-                    // TODO: Think about how we extend IFileSystem
-                    String destinationDirectory = m_fileSystem.Path.GetDirectoryName(destinationFileName);
-                    if (!m_fileSystem.Directory.Exists(destinationDirectory))
-                    {
-                        m_fileSystem.Directory.CreateDirectory(destinationDirectory);
-                    }
-
-                    m_fileSystem.File.Copy(sourceFileName, destinationFileName);
+                    m_fileSystem.SafeCopy(sourceFileName, destinationFileName, false);
                 }
             }
 
             return 0;
-        }
-
-        // TODO: Think about how we extend IFileSystem
-        public string[] GetFileNamesRecursive(string directory, string searchPattern)
-        {
-            var fileNames = new List<string>();
-            GetFileNamesRecursive(directory, searchPattern, fileNames);
-
-            return fileNames.ToArray();
-        }
-
-        // TODO: Handle access issues more gracefully
-        private void GetFileNamesRecursive(string directory, string searchPattern, List<string> fileNames)
-        {
-            String[] subDirectories = m_fileSystem.Directory.GetDirectories(directory);
-            foreach (String subDirectory in subDirectories)
-            {
-                DirectoryInfoBase di = m_fileSystem.DirectoryInfo.FromDirectoryName(subDirectory);
-                if (di.Name == "$RECYCLE.BIN" || di.Name == "System Volume Information")
-                    continue;
-
-                GetFileNamesRecursive(subDirectory, searchPattern, fileNames);
-            }
-
-            String[] files = m_fileSystem.Directory.GetFiles(directory);
-            if (files.Length > 0)
-                fileNames.AddRange(files);
         }
     }
 }
