8b7aa7394172361b428f1237eb8d6db7de2733f0
JonnyRivers <jonny.w.rivers@gmail.com>
2016-02-20 17:25:56 -0800

    Added unit test using mock file system

---
+++ Testbeds/DriveBackupTests/ApplicationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Flagstone.Logger;
+
+namespace DriveBackupTests
+{
+    [TestClass]
+    public class ApplicationTests
+    {
+        [TestMethod]
+        public void TestFileCopy()
+        {
+            ILogger logger = new ConsoleLogger();
+
+            var mockFileDataMap = new Dictionary<string, MockFileData>() {
+                { @"C:\myfile.txt", new MockFileData("Test data") }
+            };
+            IFileSystem mockFileSystem = new MockFileSystem(mockFileDataMap);
+
+            var application = new DriveBackup.Application(logger, mockFileSystem);
+
+            application.Run(@"C:\", @"D:\");
+
+            Assert.IsTrue(mockFileSystem.File.Exists(@"D:\myfile.txt"));
+        }
+    }
+}
