2fe34b8cd1ecf00f1eff3b9567613486e168e037
JonnyRivers <jonny.w.rivers@gmail.com>
2016-02-26 12:02:13 -0800

    Added last two tests

--- Testbeds/DriveBackupTests/ApplicationTests.cs
+++ Testbeds/DriveBackupTests/ApplicationTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 
@@ -14,6 +15,7 @@ namespace DriveBackupTests
         [TestMethod]
         public void TestFileCopy()
         {
+            // Arrange
             ILogger logger = new ConsoleLogger();
 
             var mockFileDataMap = new Dictionary<string, MockFileData>() {
@@ -23,9 +25,76 @@ namespace DriveBackupTests
 
             var application = new DriveBackup.Application(logger, mockFileSystem);
 
+            // Act
             application.Run(@"C:\", @"D:\");
 
+            // Assert
             Assert.IsTrue(mockFileSystem.File.Exists(@"D:\myfile.txt"));
+        }
+
+        [TestMethod]
+        public void TestOlderFileOverwritten() {
+            // Arrange
+            ILogger logger = new ConsoleLogger();
+
+            var mockFileDataMap = new Dictionary<string, MockFileData>() {
+                { @"C:\myfile.txt", new MockFileData("New data") },
+                { @"D:\myfile.txt", new MockFileData("Old data") }
+            };
+            IFileSystem mockFileSystem = new MockFileSystem(mockFileDataMap);
+            FileInfoBase newFileInfo = mockFileSystem.FileInfo.FromFileName(@"C:\myfile.txt");
+            newFileInfo.LastWriteTimeUtc = DateTime.UtcNow + new TimeSpan(0, 0, 0, 1);
+            FileInfoBase oldFileInfo = mockFileSystem.FileInfo.FromFileName(@"D:\myfile.txt");
+            oldFileInfo.LastWriteTimeUtc = DateTime.UtcNow;
+
+            var application = new DriveBackup.Application(logger, mockFileSystem);
+
+            // Act
+            application.Run(@"C:\", @"D:\");
+
+            // Assert
+            Assert.IsTrue(mockFileSystem.File.Exists(@"D:\myfile.txt"));
+            Assert.AreEqual(
+                mockFileSystem.FileInfo.FromFileName(@"C:\myfile.txt").LastWriteTimeUtc,
+                mockFileSystem.FileInfo.FromFileName(@"D:\myfile.txt").LastWriteTimeUtc
+            );
+            Assert.AreEqual(
+                mockFileSystem.File.ReadAllText(@"C:\myfile.txt"),
+                mockFileSystem.File.ReadAllText(@"D:\myfile.txt")
+            );
+        }
+
+        [TestMethod]
+        public void TestNewerFileNotOverwritten()
+        {
+            // Arrange
+            ILogger logger = new ConsoleLogger();
+
+            var mockFileDataMap = new Dictionary<string, MockFileData>() {
+                { @"C:\myfile.txt", new MockFileData("Old data") },
+                { @"D:\myfile.txt", new MockFileData("New data") }
+            };
+            IFileSystem mockFileSystem = new MockFileSystem(mockFileDataMap);
+            FileInfoBase oldFileInfo = mockFileSystem.FileInfo.FromFileName(@"C:\myfile.txt");
+            oldFileInfo.LastWriteTimeUtc = DateTime.UtcNow;
+            FileInfoBase newFileInfo = mockFileSystem.FileInfo.FromFileName(@"D:\myfile.txt");
+            newFileInfo.LastWriteTimeUtc = DateTime.UtcNow + new TimeSpan(0, 0, 0, 1);
+            
+            var application = new DriveBackup.Application(logger, mockFileSystem);
+
+            // Act
+            application.Run(@"C:\", @"D:\");
+
+            // Assert
+            Assert.IsTrue(mockFileSystem.File.Exists(@"D:\myfile.txt"));
+            Assert.AreNotEqual(
+                mockFileSystem.FileInfo.FromFileName(@"C:\myfile.txt").LastWriteTimeUtc,
+                mockFileSystem.FileInfo.FromFileName(@"D:\myfile.txt").LastWriteTimeUtc
+            );
+            Assert.AreNotEqual(
+                mockFileSystem.File.ReadAllText(@"C:\myfile.txt"),
+                mockFileSystem.File.ReadAllText(@"D:\myfile.txt")
+            );
         }
     }
 }
