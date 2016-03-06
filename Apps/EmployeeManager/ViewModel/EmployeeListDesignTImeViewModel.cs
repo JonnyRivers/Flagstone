678df820c904a4b138fc07d798b379789cb135a0
JonnyRivers <jonny.w.rivers@gmail.com>
2016-03-06 11:11:38 -0800

    Added a design time view model that uses a fake employee repository

---
+++ Apps/EmployeeManager/ViewModel/EmployeeListDesignTImeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Flagstone.Employees;
+
+namespace EmployeeManager.ViewModel
+{
+    public class EmployeeListDesignTimeViewModel : ViewModelBase
+    {
+        private readonly IEmployeeRepository m_employeeRepository;
+
+        public ObservableCollection<Employee> AllEmployees
+        {
+            get;
+            private set;
+        }
+
+        public EmployeeListDesignTimeViewModel()
+        {
+            m_employeeRepository = new FakeEmployeeRepository();
+
+            AllEmployees = new ObservableCollection<Employee>(m_employeeRepository.GetAll());
+        }
+    }
+}
---
+++ Libs/Flagstone.Employees/FakeEmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagstone.Employees
+{
+    public class FakeEmployeeRepository : IEmployeeRepository
+    {
+        private Department[] m_departments;
+        private Employee[] m_employees;
+
+        public FakeEmployeeRepository()
+        {
+            var toolsDepartment = new Department()
+            {
+                Id = 1,
+                Name = "Tools"
+            };
+
+            var jonnyRivers = new Employee()
+            {
+                FirstName = "Jonny",
+                LastName = "Rivers",
+                DateOfBirth = new DateTime(1979, 8, 16),
+                DepartmentId = toolsDepartment.Id,
+                Department = toolsDepartment
+            };
+
+            toolsDepartment.Employees = new Employee[]
+            {
+                jonnyRivers
+            };
+
+            m_departments = new Department[]
+            {
+                toolsDepartment
+            };
+
+            m_employees = new Employee[]
+            {
+                jonnyRivers
+            };
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return m_employees.ToArray();
+        }
+
+        public IEnumerable<Employee> GetByDepartmentName(string departmentName)
+        {
+            return m_employees.Where(e => e.Department.Name == departmentName).ToArray();
+        }
+    }
+}
