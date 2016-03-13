63e648fc1363e6c7c1364a51d20a8958fb2ec84a
JonnyRivers <jonny.w.rivers@gmail.com>
2016-03-12 16:56:27 -0800

    Added design time main window view model

---
+++ Apps/EmployeeManager/ViewModel/DesignTimeMainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Flagstone.Employees;
+using Flagstone.WPF;
+
+namespace EmployeeManager.ViewModel
+{
+    public class DesignTimeMainWindowViewModel : ViewModelBase
+    {
+        public DesignTimeMainWindowViewModel()
+        {
+            EmployeeListViewModel = new DesignTimeEmployeeListViewModel();
+        }
+
+        public ViewModelBase EmployeeListViewModel
+        {
+            get;
+            private set;
+        }
+    }
+}
