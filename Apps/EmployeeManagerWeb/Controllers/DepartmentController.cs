fe200de193696c9821907ad76d644b0e75a6c14c
JonnyRivers <jonny.w.rivers@gmail.com>
2016-05-08 17:53:49 -0700

    Added full, albeit simple, CRUD for departments

--- Apps/EmployeeManagerWeb/Controllers/DepartmentController.cs
+++ Apps/EmployeeManagerWeb/Controllers/DepartmentController.cs
@@ -29,5 +29,56 @@ namespace EmployeeManagerWeb.Controllers
         {
             return View(m_unitOfWork.Departments.GetAll());
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            // Directly using the Department like this seems wrong.
+            // We need to support validation.
+            Department storedDepartment = m_unitOfWork.Departments.Get(id);
+            return View(storedDepartment);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Department editedDepartment)
+        {
+            if(editedDepartment.Id == 0)
+            {
+                Department newDepartment = new Department
+                {
+                    Name = editedDepartment.Name
+                };
+                m_unitOfWork.Departments.Add(newDepartment);
+            }
+            else
+            {
+                Department storedDepartment = m_unitOfWork.Departments.Get(editedDepartment.Id);
+                storedDepartment.Name = editedDepartment.Name;
+            }
+
+            m_unitOfWork.Complete();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            Department newDepartment = new Department
+            {
+                Name = "New Department"
+            };
+
+            return View("Edit", newDepartment);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Department storedDepartment = m_unitOfWork.Departments.Get(id);
+            m_unitOfWork.Departments.Remove(storedDepartment);
+            m_unitOfWork.Complete();
+
+            return RedirectToAction("Index");
+        }
     }
 }
