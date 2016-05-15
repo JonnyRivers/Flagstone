683a5d9639bdd19fe19b5287f078222252189d4d
JonnyRivers <jonny.w.rivers@gmail.com>
2016-05-14 17:10:59 -0700

    Added readonly WebApi support for Departments

---
+++ Apps/EmployeeManagerWebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Flagstone.Data.Employees;
+
+namespace EmployeeManagerWebApi.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        private IUnitOfWorkFactory m_unitOfWorkFactory;
+
+        public DepartmentController()
+        {
+            // TODO - replace this with Ninject goodness
+            m_unitOfWorkFactory = new FakeUnitOfWorkFactory();
+        }
+
+        public DepartmentController(IUnitOfWorkFactory unitOfWorkFactory)
+        {
+            if (unitOfWorkFactory == null)
+                throw new ArgumentNullException(nameof(unitOfWorkFactory));
+
+            m_unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public IEnumerable<Models.Department> Get()
+        {
+            using (IUnitOfWork unitofWork = m_unitOfWorkFactory.Create()) {
+                return unitofWork.Departments.GetAll().Select(DbEntityToApiEntity);
+            }
+        }
+
+        public Models.Department Get(long id)
+        {
+            using (IUnitOfWork unitofWork = m_unitOfWorkFactory.Create()) {
+                return DbEntityToApiEntity(unitofWork.Departments.Get(id));
+            }
+        }
+
+        private static Models.Department DbEntityToApiEntity(Flagstone.Data.Employees.Department dbEntity)
+        {
+            return new Models.Department
+            {
+                DepartmentId = dbEntity.DepartmentId,
+                Name = dbEntity.Name
+            };
+        }
+    }
+}
---
+++ Apps/EmployeeManagerWebApi/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+using Ninject;
+
+using Flagstone.Data.Employees;
+
+namespace EmployeeManagerWebApi.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel m_kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            m_kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return m_kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return m_kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+#if DEBUG
+            m_kernel.Bind<IUnitOfWorkFactory>().To<FakeUnitOfWorkFactory>().InSingletonScope();
+#else
+            m_kernel.Bind<IUnitOfWorkFactory>().To<EFUnitOfWorkFactory>();
+#endif
+        }
+    }
+}
